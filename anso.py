218d98aae6d3d8a22d00638961e53229c4d9a1e0
Jon Lees <jonglees@gmail.com>
2026-01-24 18:54:05 +0000

    Update and rename ohe.py to anso.py

--- anso.py
+++ anso.py
@@ -9,3 +9,12 @@ def one_hot_encode(seq):
 
 print(one_hot_encode('ACGU'))
 print(len(one_hot_encode('ACGU')))
+
+
+import seaborn as sns
+dna_positions=[]
+importance_scores=[]
+for dna_position, importance in enumerate(model.feature_importances_):
+    dna_positions.append(dna_position)
+    importance_scores.append(importance)
+sns.scatterplot(x=dna_positions, y=importance_scores)
