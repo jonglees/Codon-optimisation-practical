77c556ff48caa3b9df3549d8873db52dd27384fa
Jon Lees <jonglees@gmail.com>
2026-01-24 18:49:28 +0000

    Create ohe.py

---
+++ ohe.py
@@ -0,0 +1,11 @@
+def one_hot_encode(seq):
+    rna_ohe= {'A': 0, 'U': 1, 'G': 2, 'C': 3}
+    one_hot_encoded = []
+    for nucleotide in seq:
+        rep = np.zeros(4)
+        rep[rna_ohe[nucleotide]] = 1
+        one_hot_encoded += (list(rep))
+    return one_hot_encoded
+
+print(one_hot_encode('ACGU'))
+print(len(one_hot_encode('ACGU')))
